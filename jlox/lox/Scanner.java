91e7b35de248390bec64b6b3e679e40b36052b23
Bani Somo <banisomo2003@gmail.com>
2024-06-24 21:40:53 -0500

    Implement parser

--- jlox/lox/Scanner.java
+++ jlox/lox/Scanner.java
@@ -1,7 +1,10 @@
+package lox;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import static lox.TokenType.*;
 
 public class Scanner {
     private final String source;
@@ -15,22 +18,22 @@ public class Scanner {
 
     static {
         keywords = new HashMap<>();
-        keywords.put("and", TokenType.AND);
-        keywords.put("class", TokenType.CLASS);
-        keywords.put("else", TokenType.ELSE);
-        keywords.put("false", TokenType.FALSE);
-        keywords.put("for", TokenType.FOR);
-        keywords.put("fun", TokenType.FUN);
-        keywords.put("if", TokenType.IF);
-        keywords.put("nil", TokenType.NIL);
-        keywords.put("or", TokenType.OR);
-        keywords.put("print", TokenType.PRINT);
-        keywords.put("return", TokenType.RETURN);
-        keywords.put("super", TokenType.SUPER);
-        keywords.put("this", TokenType.THIS);
-        keywords.put("true", TokenType.TRUE);
-        keywords.put("var", TokenType.VAR);
-        keywords.put("while", TokenType.WHILE);
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
     }
 
     Scanner(String source) {
@@ -43,35 +46,35 @@ public class Scanner {
             scanToken();
         }
 
-        tokens.add(new Token(TokenType.EOF, "", null, line));
+        tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
 
     private void scanToken() {
         char c = advance();
         switch (c) {
-            case '(': addToken(TokenType.LEFT_PAREN); break;
-            case ')': addToken(TokenType.RIGHT_PAREN); break;
-            case '{': addToken(TokenType.LEFT_BRACE); break;
-            case '}': addToken(TokenType.RIGHT_BRACE); break;
-            case '.': addToken(TokenType.DOT); break;
-            case ',': addToken(TokenType.COMMA); break;
-            case '+': addToken(TokenType.PLUS); break;
-            case '-': addToken(TokenType.MINUS); break;
-            case ';': addToken(TokenType.SEMICOLON); break;
-            case '*': addToken(TokenType.STAR); break;
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case '.': addToken(DOT); break;
+            case ',': addToken(COMMA); break;
+            case '+': addToken(PLUS); break;
+            case '-': addToken(MINUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
 
             case '!':
-                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                addToken(match('=') ? BANG_EQUAL : BANG);
                 break;
             case '=':
-                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
                 break;
             case '>':
-                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
             case '<':
-                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                addToken(match('=') ? LESS_EQUAL : LESS);
                 break;
 
             case '/':
@@ -81,7 +84,7 @@ public class Scanner {
                 } else if (match('*')) {
                     multilineComment();
                 } else {
-                    addToken(TokenType.SLASH);
+                    addToken(SLASH);
                 }
                 break;
 
@@ -128,7 +131,7 @@ public class Scanner {
 
         // Trim quotes
         String value = source.substring(start + 1, current - 1);
-        addToken(TokenType.STRING, value);
+        addToken(STRING, value);
     }
 
     private void number() {
@@ -142,7 +145,7 @@ public class Scanner {
                 advance();
         }
 
-        addToken(TokenType.NUMBER,
+        addToken(NUMBER,
                 Double.parseDouble(source.substring(start, current))
         );
     }
@@ -154,7 +157,7 @@ public class Scanner {
         String text = source.substring(start, current);
         TokenType type = keywords.get(text);
         if (type == null)
-            type = TokenType.IDENTIFIER;
+            type = IDENTIFIER;
         addToken(type);
     }
 
