da9c7950e48a7a6681c786e44533045123689ce6
Bani Somo <banisomo2003@gmail.com>
2024-07-13 10:41:12 -0500

    Add constructors to classes

--- jlox/lox/Interpreter.java
+++ jlox/lox/Interpreter.java
@@ -240,7 +240,7 @@ public class Interpreter implements Expr.Visitor<Object>,
 
         Map<String, LoxFunction> methods = new HashMap<>();
         for (Stmt.Function method : stmt.methods) {
-            LoxFunction function = new LoxFunction(method, environment);
+            LoxFunction function = new LoxFunction(method, environment, method.name.lexeme.equals("init"));
             methods.put(method.name.lexeme, function);
         }
 
@@ -257,7 +257,7 @@ public class Interpreter implements Expr.Visitor<Object>,
 
     @Override
     public Void visitFunctionStmt(Stmt.Function stmt) {
-        LoxFunction function = new LoxFunction(stmt, environment);
+        LoxFunction function = new LoxFunction(stmt, environment, false);
         environment.define(stmt.name.lexeme, function);
         return null;
     }
