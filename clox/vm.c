5e08735a01fa3b7cace9d814ae3d803d2927dbb2
Bani Somo <banisomo2003@gmail.com>
2024-07-29 19:46:22 -0500

    Implement scanner

--- clox/vm.c
+++ clox/vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "includes/common.h"
+#include "includes/compiler.h"
 #include "includes/debug.h"
 #include "includes/vm.h"
 
@@ -36,67 +37,54 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
-    do { \
+#define BINARY_OP(op)     \
+    do                    \
+    {                     \
         double b = pop(); \
         double a = pop(); \
-        push(a op b); \
+        push(a op b);     \
     } while (false)
 
     for (;;)
     {
-        #ifdef DEBUG_TRACE_EXECUTION
-            printf("        ");
-            for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
-            {
-                printf("[ ");
-                printValue(*slot);
-                printf(" ]");
-            }
-            printf("\n");
-            disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
-        #endif
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
 
         uint8_t instruction;
         switch (instruction = READ_BYTE())
         {
-            case OP_CONSTANT:
-            {
-                Value constant = READ_CONSTANT();
-                push(constant);
-                break;
-            }
-            case OP_ADD:
-            {
-                BINARY_OP(+);
-                break;
-            }
-            case OP_SUBTRACT:
-            {
-                BINARY_OP(-);
-                break;
-            }
-            case OP_MULTIPLY:
-            {
-                BINARY_OP(*);
-                break;
-            }
-            case OP_DIVIDE:
-            {
-                BINARY_OP(/);
-                break;
-            }
-            case OP_NEGATE:
-            {
-                push(-pop());
-                break;
-            }
-            case OP_RETURN:
-            {
-                printValue(pop());
-                printf("\n");
-                return INTERPRET_OK;
-            }
+        case OP_CONSTANT:
+            Value constant = READ_CONSTANT();
+            push(constant);
+            break;
+        case OP_ADD:
+            BINARY_OP(+);
+            break;
+        case OP_SUBTRACT:
+            BINARY_OP(-);
+            break;
+        case OP_MULTIPLY:
+            BINARY_OP(*);
+            break;
+        case OP_DIVIDE:
+            BINARY_OP(/);
+            break;
+        case OP_NEGATE:
+            push(-pop());
+            break;
+        case OP_RETURN:
+            printValue(pop());
+            printf("\n");
+            return INTERPRET_OK;
         }
     }
 
@@ -105,9 +93,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
