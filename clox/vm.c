ca674074afc3a63a00fc0be4cd8ea858311a06ee
Bani Somo <banisomo2003@gmail.com>
2024-11-18 11:56:25 -0600

    Add print and expression statements Implement error synchronization

--- clox/vm.c
+++ clox/vm.c
@@ -129,6 +129,9 @@ static InterpretResult run()
         case OP_FALSE:
             push(BOOL_VAL(false));
             break;
+        case OP_POP:
+            pop();
+            break;
         case OP_EQUAL:
         {
             Value b = pop();
@@ -181,9 +184,13 @@ static InterpretResult run()
             }
             push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
-        case OP_RETURN:
+        case OP_PRINT:
+        {
             printValue(pop());
             printf("\n");
+            break;
+        }
+        default:
             return INTERPRET_OK;
         }
     }
